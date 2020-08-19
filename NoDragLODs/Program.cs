df2054ee3de724f5162073ce2275c08c890bc504
JD Smith <jdsmith2816@gmail.com>
2020-08-18 19:57:22 -0500

    Initial commit

---
+++ NoDragLODs/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Synthesis;
+using Mutagen.Bethesda.Skyrim;
+using Octodiff.Diagnostics;
+
+namespace NoDragLODs
+{
+    public static class MyExtensions
+    {
+        public static bool ContainsInsensitive(this string str, string rhs)
+        {
+            return str.Contains(rhs, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            return SynthesisPipeline.Instance.Patch<ISkyrimMod, ISkyrimModGetter>(
+                args: args,
+                patcher: RunPatch,
+                new UserPreferences()
+                {
+                    ActionsForEmptyArgs = new RunDefaultPatcher()
+                    {
+                        IdentifyingModKey = "NoDragonLODs.esp",
+                        TargetRelease = GameRelease.SkyrimSE
+                    }
+                }
+                );
+        }
+
+        public static void RunPatch(SynthesisState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            foreach(var npc in state.LoadOrder.PriorityOrder.WinningOverrides<INpcGetter>())
+            {
+                if (npc.PlayerSkills.FarAwayModelDistance == 0) continue;
+
+                if (!npc.Race.TryResolve(state.LinkCache, out var race)) continue;
+
+                if (race.SkeletalModel.Male?.File.ContainsInsensitive("actors\\dragon\\") == false) continue;
+
+                var modifiedNpc = state.PatchMod.Npcs.GetOrAddAsOverride(npc);
+                modifiedNpc.PlayerSkills.FarAwayModelDistance = 0;
+            }
+        }
+    }
+}
